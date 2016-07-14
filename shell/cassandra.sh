2926aa25d5e1bcf24c25bdc244fc9e52842e610f
colddew <88028842@qq.com>
2016-07-15 00:18:56 +0800

    modify cassandra.sh

--- shell/cassandra.sh
+++ shell/cassandra.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # start cassandra
-/Users/tools/cassandra/bin/cassandra -f
+# /Users/tools/cassandra/bin/cassandra -f
+/Users/tools/cassandra/bin/cassandra
 
 # kill cassandra
 # ps aux | grep cassandra | grep -v grep | awk '{print $2}' | xargs kill -9
