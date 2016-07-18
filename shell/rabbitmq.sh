5eb850759e6c6c355a8d797c4c5c338489d59361
colddew <88028842@qq.com>
2016-07-19 00:09:31 +0800

    modify rabbitmq.sh

--- shell/rabbitmq.sh
+++ shell/rabbitmq.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # start rabbitmq
 /Users/tools/rabbitmq/sbin/rabbitmq-server &
+/Users/tools/rabbitmq/sbin/rabbitmq-server restart &
 
 # stop rabbitmq
 ps aux | grep rabbitmq | grep -v grep | awk '{print $2}' | xargs kill -9
