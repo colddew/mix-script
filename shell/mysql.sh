9bcae47c65381d21c27614610b9cd183f56a8063
colddew <88028842@qq.com>
2016-07-23 14:57:16 +0800

    modify mysql.sh

--- shell/mysql.sh
+++ shell/mysql.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+# startup
+# echo $1 | sudo -S /usr/local/mysql/bin/mysqld_safe &
 sudo /usr/local/mysql/bin/mysqld_safe &
+
+# shutdown
+ps aux | grep mysql | grep -v grep | awk '{print $2}' | xargs sudo kill -9
