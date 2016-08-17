ff36e3b05596795389c77b7b3bc453be5b1a6de7
colddew <88028842@qq.com>
2016-08-18 03:19:07 +0800

    add partial kibana shell script

--- shell/elasticsearch.sh
+++ shell/elasticsearch.sh
@@ -13,3 +13,9 @@
 # install plugin
 # /Users/tools/es-cluster/es-node1/bin/plugin install mobz/elasticsearch-head
 # 127.0.0.1:9201/_plugin/head/
+
+# /Users/tools/es-cluster/es-node1/bin/plugin install license
+# /Users/tools/es-cluster/es-node1/bin/plugin install marvel-agent
+# /Users/tools/kibana/bin/kibana plugin --install elasticsearch/marvel/latest
+# http://127.0.0.1:5601/app/kibana
+# http://127.0.0.1:5601/app/marvel
