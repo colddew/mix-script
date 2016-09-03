4c0ea55c2044dd190c740e5b22916deea0a866e7
colddew <88028842@qq.com>
2016-09-03 13:03:28 +0800

    add at and cron shell script

---
+++ shell/linux.sh
@@ -0,0 +1,14 @@
+# at
+at now + 30 minutes
+# command
+# ctrl + D
+atq
+atrm <command id>
+
+# cron
+service crond start
+service crond status
+crontab -e
+# 0/1 * * * * <command>
+crontab -l
+crontab -r
