2a663fa49261f5236fa6c4677cc509ea73da4682
colddew <88028842@qq.com>
2016-10-18 13:42:42 +0800

    modify kafka and zookeeper script

--- shell/zookeeper.sh
+++ shell/zookeeper.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-/Users/tools/zookeeper/bin/zkServer.sh start
+# start zookeeper
+/Users/tools/zookeeper/bin/zkServer.sh start /Users/tools/zookeeper/conf/zoo.cfg &
+
+# shutdown zookeeper
+ps aux | grep zookeeper | grep -v grep | awk '{print $2}' | xargs kill -9
