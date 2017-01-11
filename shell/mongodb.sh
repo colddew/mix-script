2fdbd5d576dd7d7a876479824a321df31a63b9f7
colddew <88028842@qq.com>
2017-01-11 12:15:18 +0800

    add mongo script

--- shell/mongodb.sh
+++ shell/mongodb.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
-# start mongo
+# single mongo
 /usr/local/bin/mongod -config /usr/local/etc/mongod.conf
+
+# mongo cluster
+
+# replica sets
+/usr/local/bin/mongod --dbpath /usr/local/var/mongodb --replSet repset
+mongo --host <mongo-host> --port <mongo-port>
+config = { _id:"repset", members:[
+  {_id:0,host:"<mongo-host-1>:<mongo-port>"},
+  {_id:1,host:"<mongo-host-2>:<mongo-port>"},
+  {_id:2,host:"<mongo-host-1>:<mongo-port>"}]
+}
+rs.initiate(config);
+rs.status();
+db.getMongo().setSlaveOk();
+rs.conf();
 
 # monitor
 /usr/local/bin/mongostat --host=<mongo-server-ip> --port=27017 5
@@ -16,10 +31,10 @@ mongosync -h 127.0.0.1:27017 --to 127.0.0.1:27018 -d contact -c contactInfos --o
 
 # command
 mongo --host <mongo-host> --port <mongo-port>
-show dbs
-use <db>
-show collections
-db.<collection>.count()
-db.<collection>.find({})
-db.currentOP()
-db.version()
+show dbs;
+use <db>;
+show collections;
+db.<collection>.count();
+db.<collection>.find({});
+db.currentOP();
+db.version();
