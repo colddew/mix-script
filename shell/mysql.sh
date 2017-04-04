d3513f494c73d53d81ff2af895db9e1a0b6dc275
colddew <88028842@qq.com>
2017-04-04 13:57:08 +0800

    add mysql script

--- shell/mysql.sh
+++ shell/mysql.sh
@@ -5,3 +5,7 @@ sudo /usr/local/mysql/bin/mysqld_safe
 
 # shutdown
 ps aux | grep mysql | grep -v grep | awk '{print $2}' | xargs sudo kill -9
+
+# assign remote access privilege
+GRANT ALL ON *.* to root@'192.168.xxx.xxx' IDENTIFIED BY 'password';
+FLUSH PRIVILEGES;
