e2f4659d3772539a8a54620a086bb46204489c74
colddew <88028842@qq.com>
2017-04-12 16:07:21 +0800

    add hexo script

--- shell/hexo.sh
+++ shell/hexo.sh
@@ -2,11 +2,14 @@ npm install hexo-cli -g
 hexo init blog
 cd blog
 npm install --no-optional
-hexo server
+hexo server [-p 8888 -i 192.168.1.1]
 
 hexo new "New Post"
-hexo generate
+hexo generate --watch
+npm install hexo-deployer-git --save
 hexo deploy
+# hexo g -d
 
 hexo clean
+rm -rf .deploy_git
 hexo list
