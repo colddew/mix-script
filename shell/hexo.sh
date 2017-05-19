40e739b2a94ea9333d87e975587b141e1cbd2a19
colddew <88028842@qq.com>
2017-05-19 12:53:16 +0800

    update hexo script

--- shell/hexo.sh
+++ shell/hexo.sh
@@ -2,14 +2,15 @@ npm install hexo-cli -g
 hexo init blog
 cd blog
 npm install --no-optional
-hexo server [-p 8888 -i 192.168.1.1]
+hexo s[erver] [-p 8888 -i 192.168.1.1]
 
 hexo new "New Post"
-hexo generate --watch
+hexo g[enerate] --watch
 npm install hexo-deployer-git --save
-hexo deploy
+hexo d[eploy]
 # hexo g -d
 
+# clean cache files (db.json) and static files (public) 
 hexo clean
 rm -rf .deploy_git
 hexo list
