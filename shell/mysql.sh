2dc0bb29493d3700bbaceb3686bda22d6c2ad9d3
colddew <88028842@qq.com>
2017-06-10 17:10:58 +0800

    add mysql script

--- shell/mysql.sh
+++ shell/mysql.sh
@@ -9,3 +9,16 @@ ps aux | grep mysql | grep -v grep | awk '{print $2}' | xargs sudo kill -9
 # assign remote access privilege
 GRANT ALL ON *.* to root@'192.168.xxx.xxx' IDENTIFIED BY 'password';
 FLUSH PRIVILEGES;
+
+# SET NAMES 'utf8mb4';  
+# SET character_set_client = utf8mb4;  
+# SET character_set_results = utf8mb4;   
+# SET character_set_connection = utf8mb4;
+
+# SHOW VARIABLES LIKE 'character%';
+SHOW VARIABLES WHERE Variable_name LIKE 'character\_set\_%' OR Variable_name LIKE 'collation%';
+
+# change character set to utf8mb4
+ALTER DATABASE succulent CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+ALTER TABLE T_Succulent_User CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE T_Succulent_User CHANGE NickName NickName varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci COMMENT 'XXX';
