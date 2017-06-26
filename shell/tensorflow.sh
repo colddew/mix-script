7e1fd9bb3c19530d6e6a092d4414867403c166cf
colddew <88028842@qq.com>
2017-06-27 00:43:47 +0800

    add tensorflow script

--- shell/tensorflow.sh
+++ shell/tensorflow.sh
@@ -1,13 +1,24 @@
-# install tensorflow
+# install tensorflow on Mac OSX
 sudo easy_install pip
 virtualenv --system-site-packages ~/tensorflow
-source ~/tensorflow/bin/activate
+# source ~/tensorflow/bin/activate
+# deactivate
 easy_install -U pip
 pip install --upgrade tensorflow
 # pip install --upgrade https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.2.0-py2-none-any.whl
 
+# install tensorflow from sources
+brew install bazel
+# bazel version
+sudo pip install six numpy wheel
+git clone https://github.com/tensorflow/tensorflow
+cd tensorflow
+./configure
+bazel build --config=opt //tensorflow/tools/pip_package:build_pip_package
+bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
+sudo pip install /tmp/tensorflow_pkg/tensorflow-1.2.0-cp27-cp27m-macosx_10_12_x86_64.whl
+
 # uninstall tensorflow
-# deactivate
 # pip uninstall tensorflow
 # rm -r ~/tensorflow
 
@@ -16,3 +27,19 @@ import tensorflow as tf
 hello = tf.constant('Hello, TensorFlow!')
 sess = tf.Session()
 print(sess.run(hello))
+
+# retrain Inception's final layer for new categories
+cd ~
+curl -O http://download.tensorflow.org/example_images/flower_photos.tgz
+tar xzf flower_photos.tgz
+tensorboard --logdir /tmp/retrain_logs
+# http://localhost:6006
+bazel build tensorflow/examples/image_retraining:retrain
+bazel-bin/tensorflow/examples/image_retraining/retrain --image_dir ~/Downloads/flower_photos
+bazel-bin/tensorflow/examples/label_image/label_image --image=$HOME/Downloads/flower_photos/daisy/21652746_cc379e0eea_m.jpg
+
+bazel build tensorflow/examples/label_image:label_image && \
+bazel-bin/tensorflow/examples/label_image/label_image \
+--graph=/tmp/output_graph.pb --labels=/tmp/output_labels.txt \
+--output_layer=final_result --input_layer=Mul \
+--image=$HOME/Downloads/flower_photos/daisy/21652746_cc379e0eea_m.jpg
