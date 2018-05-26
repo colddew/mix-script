4d13759e0087eacc7b2e92f9a8f7e473e7db696e
colddew <88028842@qq.com>
2018-05-26 23:35:08 +0800

    add consul shell script

--- shell/consul.agent.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-consul agent -data-dir /tmp/consul -node=<local-ip> -bind=<local-ip> -join=<remote-consul-server>
-consul members
---
+++ shell/consul.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# consul server cluster
+ip addr show docker0
+# /etc/default/docker
+DOCKER_OPTS="--dns 172.17.0.1 --dns 8.8.8.8 --dns-search service .consul"
+service docker restart
+docker run -d -h larry -p 8300:8300 -p 8301:8301 -p 8301:8301/udp -p 8302:8302 -p 8302:8302/udp -p 8400:8400 -p 8500:8500 -p 53:53/udp --name larry_agent consul -server -advertise 101.132.146.94 -bootstrap-expect 2
+docker run -d -h curly -p 8300:8300 -p 8301:8301 -p 8301:8301/udp -p 8302:8302 -p 8302:8302/udp -p 8400:8400 -p 8500:8500 -p 53:53/udp --name curly_agent consul -server -advertise 122.152.199.200 -join 101.132.146.94
+docker logs larry_agent
+docker logs curely_agent
+# http://101.132.146.94:8500
+# http://localhost:8500/v1/health/service/consul?pretty
+dig @localhost -p 8600 consul.service.consul
+
+# development mode
+docker run -d --name=dev-consul -e CONSUL_BIND_INTERFACE=eth0 consul
+docker run -d -e CONSUL_BIND_INTERFACE=eth0 consul agent -dev -join=172.17.0.2
+docker run -d -e CONSUL_BIND_INTERFACE=eth0 consul agent -dev -join=172.17.0.2
+docker exec -t dev-consul consul members
+
+# consul agent
+consul agent -data-dir /tmp/consul -node=<local-ip> -bind=<local-ip> -join=<remote-consul-server>
+consul members
