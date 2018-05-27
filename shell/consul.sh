e0e32159e61b5098b27f745ae797377cc0695c42
colddew <88028842@qq.com>
2018-05-27 15:45:21 +0800

    add consul shell script

--- shell/consul.sh
+++ shell/consul.sh
@@ -12,11 +12,11 @@ docker logs curely_agent
 # http://localhost:8500/v1/health/service/consul?pretty
 dig @localhost -p 8600 consul.service.consul
 
-# development mode
-docker run -d --name=dev-consul -e CONSUL_BIND_INTERFACE=eth0 consul
-docker run -d -e CONSUL_BIND_INTERFACE=eth0 consul agent -dev -join=172.17.0.2
-docker run -d -e CONSUL_BIND_INTERFACE=eth0 consul agent -dev -join=172.17.0.2
-docker exec -t dev-consul consul members
+# consul server agent and client agent
+docker run -d -p 8300:8300 -p 8301:8301 -p 8301:8301/udp -p 8302:8302 -p 8302:8302/udp -p 8400:8400 -p 8500:8500 -p 53:53/udp -e CONSUL_UI_BETA=true -h node1 --name node1 consul agent -server -bootstrap-expect=1 -node=node1 -client 0.0.0.0 -ui -datacenter=dc1
+# JOIN_IP="$(docker inspect -f '{{.NetworkSettings.IPAddress}}' node1)"
+docker run -d --name client1 -h client1 consul agent -node=client1 -join 172.17.0.2 -datacenter=dc1
+docker run -d --name client2 -h client2 consul agent -node=client2 -join 172.17.0.2 -datacenter=dc1
 
 # consul agent
 consul agent -data-dir /tmp/consul -node=<local-ip> -bind=<local-ip> -join=<remote-consul-server>
