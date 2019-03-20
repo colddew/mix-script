4447bba3c69c0e7b1f4ec78c8d4eb46d0ffae977
colddew <88028842@qq.com>
2019-03-21 00:03:08 +0800

    update docker.sh

--- shell/docker.sh
+++ shell/docker.sh
@@ -82,3 +82,8 @@ docker-compose logs
 docker-compose stop
 docker-compose start
 docker-compose rm
+
+# track docker logs
+pred='process matches ".*(ocker|vpnkit).*" || (process in {"taskgated-helper", "launchservicesd", "kernel"} && eventMessage contains[c] "docker")'
+/usr/bin/log stream --style syslog --level=debug --color=always --predicate "$pred"
+
