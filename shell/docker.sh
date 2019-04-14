1a9735fcfe990d3738e1507af698344e85d857e1
colddew <88028842@qq.com>
2019-04-14 23:56:35 +0800

    update docker script

--- shell/docker.sh
+++ shell/docker.sh
@@ -14,7 +14,7 @@ docker ps -l
 docker ps -n 3
 docker images
 docke image rm nginx
-// docker rmi nginx
+# docker rmi nginx
 docker rmi `docker images -a -q`
 
 docker start webserver
@@ -24,13 +24,13 @@ docker kill webserver
 docker [container] rm webserver
 docker rm -f webserver 
 docker rm `docker ps -a -q`
-// docker attach daemon_ubuntu
+# docker attach daemon_ubuntu
 docker logs --tail 10 -tf daemon_ubuntu
 docker top daemon_ubuntu
 docker stats daemon_ubuntu
 docker inspect daemon_ubuntu
-// control + c
-// docker exec -i -t daemon_ubuntu /bin/bash
+# control + c
+# docker exec -i -t daemon_ubuntu /bin/bash
 docker exec -d daemon_ubuntu touch /tmp/tmp.txt
 docker search puppet
 docker pull ubuntu:12.04
@@ -50,7 +50,7 @@ docker network disconnect <network> <container-name>
 
 docker run hello-world
 docker run -d -p 80:80 --name webserver nginx
-// docker run -i -t ubuntu /bin/bash
+# docker run -i -t ubuntu /bin/bash
 docker run --restart=always --name daemon_ubuntu -d ubuntu /bin/sh -c "while true; do echo hello world; sleep 1; done"
 docker run -i -t ubuntu:12.04 /bin/bash
 
