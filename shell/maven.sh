6139fc2c4a4fb426e57f85fdd18160526ff33956
colddew <88028842@qq.com>
2019-05-25 13:45:35 +0800

    add maven script

--- shell/maven.sh
+++ shell/maven.sh
@@ -3,6 +3,8 @@ mvn -v
 # create project
 # mvn <plugin:goal>
 mvn archetype:generate -DgroupId=<groupId> -DartifactId=<artifactId> -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=<version> -DinteractiveMode=false
+mvn archetype:generate -DgroupId=cn.plantlink -DartifactId=<artifactId> -DarchetypeArtifactId=micro-service-archetype -DarchetypeVersion=<version> -DinteractiveMode=false
+mvn archetype:generate -B -DarchetypeGroupId=<archetypeGroupId> -DarchetypeArtifactId=<archetypeArtifactId> -DarchetypeVersion=<archetypeVersion> -DarchetypeCatalog=http://<nexus-server-ip>:8081/nexus/content/groups/public/archetype-catalog.xml -DgroupId=<groupId> -DartifactId=<rtifactId> -Dpackage=com.plantlink.<micro-service-name>
 
 # lifecycles
 # mvn <phase>
