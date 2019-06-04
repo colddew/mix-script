da0b26960109bf0cb6cfe9147fab9897bb10a682
colddew <88028842@qq.com>
2019-06-04 23:43:57 +0800

    update zookeeper script

--- shell/zookeeper.sh
+++ shell/zookeeper.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 # start zookeeper
-<zookeeper-home>/bin/zkServer.sh start <zookeeper-home>/conf/zoo.cfg &
+<zookeeper-home>/bin/zkServer.sh start &
 
 # shutdown zookeeper
 ps aux | grep zookeeper | grep -v grep | awk '{print $2}' | xargs kill -9
+
+# client
+<zk-home>/bin/zkCli.sh -server 127.0.0.1:2181
+ls /
+create /zk_test my_data
+get /zk_test
+set /zk_test junk
+get /zk_test
+delete /zk_test
