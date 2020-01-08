966d01e3fcd804f660b6aedf6fe5da26c986e4d3
colddew <88028842@qq.com>
2020-01-08 19:15:48 +0800

    add nginx script

--- shell/nginx.sh
+++ shell/nginx.sh
@@ -1,6 +1,27 @@
-#!/bin/sh
-#/usr/local/bin/nginx -t
-#/usr/local/bin/nginx -s stop
-#/usr/local/bin/nginx -s reload
+# basic config
+yum install -y gcc-c++
+yum install -y pcre pcre-devel
+yum install -y zlib zlib-devel
+yum install -y openssl openssl-devel
+yum install -y lrzsz
 
-/usr/local/bin/nginx
+# install
+curl -O http://nginx.org/download/nginx-1.17.7.tar.gz
+tar -zxvf nginx-1.17.7.tar.gz
+mv nginx-1.17.7 /usr/local
+ln -s nginx-1.17.7 nginx
+./configure
+make & make install
+
+# start & stop
+#!/bin/sh
+/usr/local/nginx/sbin/nginx           # start  
+/usr/local/nginx/sbin/nginx -t        # test config
+/usr/local/nginx/sbin/nginx -v        # show version
+/usr/local/nginx/sbin/nginx -s stop   # fast stop
+/usr/local/nginx/sbin/nginx -s quit   # normal stop
+/usr/local/nginx/sbin/nginx -s reload # restart
+
+# firewall
+# firewall-cmd --zone=public --add-port=80/tcp --permanent
+# firewall-cmd --reload
