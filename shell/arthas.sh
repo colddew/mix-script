e5845d1f012c70155f2e95065e5a9cbfc3284640
colddew <88028842@qq.com>
2020-06-22 01:58:24 +0800

    add arthas script

--- shell/arthas.sh
+++ shell/arthas.sh
@@ -7,15 +7,18 @@ dashboard
 thread <pid>
 thread -n 5
 thread -b
-sc -d *<ClassName>
-jad <package.ClassName>
 jvm
 watch <package.ClassName> <MethodName> returnObj
+watch <package.ClassName> <MethodName> {params,returnObj}
 watch <package.ClassName> <MethodName> {params[0], throwExp} -e
-trace <package.ClassName> <MethodName> '$cost>100'
-trace -j <package.ClassName> <MethodName>
-tt -t <package.ClassName> <MethodName
+trace <package.ClassName> <MethodName> '#cost>100'
+tt -t <package.ClassName> <MethodName>
+tt -l
+tt -s 'method.name=="<MethodName>"'
 tt -i <tracd-id> -p
+jad <package.ClassName>
+sc -d *<ClassName>
+redefine -c <classLoaderHash> <newClassName>
 options unsafe true
 stack java.lang.System gc
 exit/quit
