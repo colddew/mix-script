4f5a1fb6299833e6acaeb5a0725b8aefbb2713d0
colddew <88028842@qq.com>
2020-06-29 00:08:48 +0800

    add mysql deadlock script

--- shell/mysql.sh
+++ shell/mysql.sh
@@ -25,3 +25,26 @@ ALTER TABLE T_Succulent_User CHANGE NickName NickName varchar(128) CHARACTER SET
 
 # mycli
 mycli <database-name>
+
+# deadlock
+show status like '%lock%';
+show processlist;
+
+select @@tx_isolation;
+select @@autocommit;
+
+SELECT * FROM INFORMATION_SCHEMA.INNODB_LOCKS;
+SELECT * FROM INFORMATION_SCHEMA.INNODB_LOCK_WAITS;
+SELECT * FROM information_schema.INNODB_TRX;
+
+select r.trx_id wait_trx_id, r.trx_mysql_thread_id wait_thr_id, r.trx_query wait_query, b.trx_id block_trx_id, b.trx_mysql_thread_id block_thrd_id, b.trx_query block_query from information_schema.innodb_lock_waits w inner join information_schema.innodb_trx b on b.trx_id = w.blocking_trx_id inner join information_schema.innodb_trx r on r.trx_id = w.requesting_trx_id;
+
+-- innodb_print_all_deadlocks  = ON
+-- innodb_status_output_locks = ON
+
+SELECT CONCAT_WS('','kill',' ',t.trx_mysql_thread_id,';')a FROM information_schema.INNODB_TRX t;
+-- kill <trx_mysql_thread_id>
+
+-- InnoDB Standard Monitor and Lock Monitor Output
+SHOW ENGINE INNODB STATUS;
+select @@log_error;
