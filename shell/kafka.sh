dbb4b500e4943e8cbee18f67bb50d8816c1bcda3
colddew <88028842@qq.com>
2020-11-04 01:56:02 +0800

    add zookeeper and kafka cluster script

--- shell/kafka.sh
+++ shell/kafka.sh
@@ -33,3 +33,42 @@ nohup <kafka-home>/bin/kafka-server-start.sh <kafka-home>/config/cluster/cluster
 # group operation
 # <kafka-home>/bin/kafka-consumer-groups.sh --describe --group microservice-group --zookeeper localhost:2181
 # <kafka-home>/bin/kafka-consumer-groups.sh --list --zookeeper localhost:2181
+
+########## kafka cluster ##########
+# start built-in zooKeeper
+nohup /usr/local/kafka/bin/zookeeper-server-start.sh /usr/local/kafka/config/zookeeper.properties &
+
+# start kafka cluster
+nohup /usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/cluster/cluster-server-1.properties &
+nohup /usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/cluster/cluster-server-2.properties &
+nohup /usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/cluster/cluster-server-3.properties &
+
+# create topic
+# /usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 2 --partitions 3 --topic test
+/usr/local/kafka/bin/kafka-topics.sh --create --bootstrap-server 172.17.248.236:9991,172.17.248.236:9992,172.17.248.236:9993 --replication-factor 2 --partitions 3 --topic test2
+
+# alter partitions
+/usr/local/kafka/bin/kafka-topics.sh --alter --bootstrap-server 172.17.248.236:9991,172.17.248.236:9992,172.17.248.236:9993 --partitions 3 --topic test2
+
+# list topic
+# /usr/local/kafka/bin/kafka-topics.sh --list --zookeeper localhost:2181
+/usr/local/kafka/bin/kafka-topics.sh --list --bootstrap-server 172.17.248.236:9991,172.17.248.236:9992,172.17.248.236:9993
+
+# describe topic
+# /usr/local/kafka/bin/kafka-topics.sh --describe --zookeeper localhost:2181 --topic test
+/usr/local/kafka/bin/kafka-topics.sh --describe --bootstrap-server 172.17.248.236:9991,172.17.248.236:9992,172.17.248.236:9993 --topic test2
+
+# delete topic
+# /usr/local/kafka/bin/kafka-topics.sh --delete -zookeeper localhost:2181 --topic test
+/usr/local/kafka/bin/kafka-topics.sh --delete --bootstrap-server 172.17.248.236:9991,172.17.248.236:9992,172.17.248.236:9993 --topic test
+
+# producer send message
+# /usr/local/kafka/bin/kafka-console-producer.sh --broker-list localhost:9991,localhost:9992,localhost:9993 --topic test
+/usr/local/kafka/bin/kafka-console-producer.sh --bootstrap-server 172.17.248.236:9991,172.17.248.236:9992,172.17.248.236:9993 --topic test
+
+# consumer consume message
+# /usr/local/kafka/bin/kafka-console-consumer.sh --zookeeper localhost:2181 --from-beginning --topic test
+/usr/local/kafka/bin/kafka-console-consumer.sh --bootstrap-server 172.17.248.236:9991,172.17.248.236:9992,172.17.248.236:9993 --group legacy --from-beginning --topic test
+
+# delete kafka data
+rm -rf /tmp/kafka-logs /tmp/zookeeper
--- shell/zookeeper.sh
+++ shell/zookeeper.sh
@@ -13,3 +13,77 @@ get /zk_test
 set /zk_test junk
 get /zk_test
 delete /zk_test
+
+########## zookeeper cluster ##########
+# start node
+/usr/local/zookeeper/bin/zkServer.sh start /usr/local/zookeeper/conf/zk1.cfg &
+/usr/local/zookeeper/bin/zkServer.sh start /usr/local/zookeeper/conf/zk2.cfg &
+/usr/local/zookeeper/bin/zkServer.sh start /usr/local/zookeeper/conf/zk3.cfg &
+
+# check node status
+/usr/local/zookeeper/bin/zkServer.sh status /usr/local/zookeeper/conf/zk1.cfg
+/usr/local/zookeeper/bin/zkServer.sh status /usr/local/zookeeper/conf/zk2.cfg
+/usr/local/zookeeper/bin/zkServer.sh status /usr/local/zookeeper/conf/zk3.cfg
+
+# stop & restart node
+/usr/local/zookeeper/bin/zkServer.sh stop /usr/local/zookeeper/conf/zk2.cfg
+/usr/local/zookeeper/bin/zkServer.sh restart /usr/local/zookeeper/conf/zk3.cfg
+
+# client link to server
+/usr/local/zookeeper/bin/zkCli.sh -server 127.0.0.1:2181
+/usr/local/zookeeper/bin/zkCli.sh -server 127.0.0.1:2182
+/usr/local/zookeeper/bin/zkCli.sh -server 127.0.0.1:2183
+
+########## cluster config ##########
+###### zk1 ##########
+# /usr/local/zookeeper/conf/zk1.cfg
+tickTime=2000
+initLimit=10
+syncLimit=5
+dataDir=/usr/local/zookeeper/zk1/data
+dataLogDir=/usr/local/zookeeper/zk1/logs
+clientPort=2181
+server.1=127.0.0.1:2881:3881
+server.2=127.0.0.1:2882:3882
+server.3=127.0.0.1:2883:3883
+
+# /usr/local/zookeeper/zk1/data/myid
+1
+
+# /usr/local/zookeeper/zk1/logs
+
+########## zk2 ##########
+
+# /usr/local/zookeeper/conf/zk2.cfg
+tickTime=2000
+initLimit=10
+syncLimit=5
+dataDir=/usr/local/zookeeper/zk2/data
+dataLogDir=/usr/local/zookeeper/zk2/logs
+clientPort=2182
+server.1=127.0.0.1:2881:3881
+server.2=127.0.0.1:2882:3882
+server.3=127.0.0.1:2883:3883
+
+# /usr/local/zookeeper/zk2/data/myid
+2
+
+# /usr/local/zookeeper/zk2/logs
+
+########## zk3 ##########
+
+# /usr/local/zookeeper/conf/zk3.cfg
+tickTime=2000
+initLimit=10
+syncLimit=5
+dataDir=/usr/local/zookeeper/zk3/data
+dataLogDir=/usr/local/zookeeper/zk3/logs
+clientPort=2183
+server.1=127.0.0.1:2881:3881
+server.2=127.0.0.1:2882:3882
+server.3=127.0.0.1:2883:3883
+
+# /usr/local/zookeeper/zk3/data/myid
+3
+
+# /usr/local/zookeeper/zk3/logs
